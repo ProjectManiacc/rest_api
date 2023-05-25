d5dddf3a33e0e5282346f6aa9f9d21c72a48c653
ProjectManiacc <piotrkluziok@gmail.com>
2023-05-25 11:17:10 +0200

    added protection to token

--- app.py
+++ app.py
@@ -29,6 +29,20 @@ def create_app(db_url=None):
     
     app.config["JWT_SECRET_KEY"] = "319067927431215366150337538778424930111"
     jwt = JWTManager(app)
+    
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+            return {"message": "Token expired"}, 401
+        
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return {"message": "Invalid token"}, 401
+    
+    @jwt.unauthorized_loader
+    def unauthorized_callback(error):
+        return {"message": "Unauthorized"}, 401
+    
+    
 
     with app.app_context():
         db.create_all()
